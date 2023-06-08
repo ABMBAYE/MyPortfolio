868a24d7e9fffc57f0e8cc331071b38c1480b8a3
Ahmadou <ahmadoubambambaye2@gmail.com>
2023-06-08 13:29:34 +0200

    Initial commit

---
+++ forms/contact.php
@@ -0,0 +1,28 @@
+<?php
+// Vérifie si le formulaire a été soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupère les données du formulaire
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Construit le contenu du message
+    $body = "Prénoms(s) : $prenom\n";
+    $body = "Nom : $nom\n";
+    $body .= "Email : $email\n";
+    $body .= "Subject : $subject\n";
+    $body .= "Message : $message\n";
+    
+    // Envoie le message
+    $success = mail('abmbaye2@gmail.com', 'Contact Form Submission', $body);
+    
+    // Vérifie si l'envoi a réussi
+    if ($success) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
